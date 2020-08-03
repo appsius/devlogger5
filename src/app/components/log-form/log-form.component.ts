5ec1b136ac1aa9b57f0aa783431054cd8dc0501c
appsius <ilyas.ustunkaya234434@gmail.com>
2020-08-03 18:41:37 +0300

    Observables & Component COmmunication

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Log } from "../../models/Log";
+import { LogService } from "../../services/log.service";
 
 @Component({
-  selector: 'app-log-form',
-  templateUrl: './log-form.component.html',
-  styleUrls: ['./log-form.component.css']
+  selector: "app-log-form",
+  templateUrl: "./log-form.component.html",
+  styleUrls: ["./log-form.component.css"],
 })
 export class LogFormComponent implements OnInit {
+  id: string;
+  text: string;
+  date: any;
 
-  constructor() { }
+  constructor(private logService: LogService) {}
 
   ngOnInit(): void {
+    this.logService.selectedLog.subscribe((log) => {
+      if (log.id !== null) {
+        this.id = log.id;
+        this.text = log.text;
+        this.date = log.date;
+      }
+    });
   }
-
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Log } from "../models/Log";
+import { BehaviorSubject, Observable, of } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -7,5 +8,42 @@ import { Log } from "../models/Log";
 export class LogService {
   logs: Log[];
 
-  constructor() {}
+  private logSourse = new BehaviorSubject<Log>({
+    id: null,
+    text: null,
+    date: null,
+  });
+  selectedLog = this.logSourse.asObservable();
+
+  constructor() {
+    this.logs = [
+      {
+        id: "1",
+        text: "Auto development setup",
+        date: new Date("02/12/2020 12:22:11"),
+      },
+      {
+        id: "2",
+        text: "Authentication done",
+        date: new Date("02/12/2020 12:22:11"),
+      },
+      {
+        id: "3",
+        text: "Security issues fixed",
+        date: new Date("02/12/2020 12:33:11"),
+      },
+    ];
+  }
+
+  getLogs() {
+    return of(
+      this.logs.sort((a, b) => {
+        return b.date - a.date;
+      })
+    );
+  }
+
+  setFormLog(log: Log) {
+    this.logSourse.next(log);
+  }
 }
